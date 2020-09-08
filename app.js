4596528356d74ba86c7c13f734dee329aaba417e
ahmed usama <ahmed.usama25@hotmail.com>
2020-09-08 21:49:17 +0500

    updated code

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 function send(){
-    var inputText= document.getElementById(input-text)
-    
+    var messageDiv = document.getElementById("messageContainer")
+    var inputText= document.getElementById("inputText")
+    var textElement= document.createElement("p")
+    var textContent= document.createTextNode(inputText.value)
+    textElement.appendChild(textContent)
+    messageDiv.appendChild(textElement)
+    console.log(textElement)
+        
 }
